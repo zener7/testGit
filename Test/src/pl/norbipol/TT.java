e384e25780a45d3d5be37c77c77cd65f81e07918
Norbert <Norbert@192.168.1.90>
2016-01-17 20:06:39 +0100

    dobra poprawka k2

--- Test/src/pl/norbipol/TT.java
+++ Test/src/pl/norbipol/TT.java
@@ -3,8 +3,7 @@ package pl.norbipol;
 public class TT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("test1hj");
+		System.out.println("jest ju¿ ok");
 	}
 
 }
